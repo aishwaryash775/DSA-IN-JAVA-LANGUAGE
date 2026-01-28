4beec4bd0578d5f80c7ada4488a2602a7589b624
Aishwarya Shinde <Aishwaryash880@gmail.com>
2026-01-28 18:22:39 +0530

    Fixed resource leak by closing Scanner

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 
 import java.util.Scanner;
 
-
 public class Main{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your Rollno: ");
         int rollno = input.nextInt();
         System.out.println("Your Rollno is: " + rollno);
-      
+      input.close();
     } 
 }
--- sum.java
+++ sum.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
+
 public class sum{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-     float num1 = input.nextFloat();
+        Scanner in = new Scanner(System.in);
+     float num1 = in.nextFloat();
 
-        float num2 = input.nextFloat();
+        float num2 = in.nextFloat();
     float sum = num1 + num2;
 System.out.println("sum is" + sum);
+in.close();
     }
 }
 
