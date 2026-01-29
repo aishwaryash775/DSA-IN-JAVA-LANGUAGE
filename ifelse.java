7bde25c355253ea0d2f624add09b28a66c2b7344
Aishwarya Shinde <Aishwaryash880@gmail.com>
2026-01-29 21:10:30 +0530

    if else condition learn

---
+++ ifelse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ifelse{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your salary:");
+        int salary = sc.nextInt();
+        if ( salary < 10000){
+            System.out.println("I am student");
+
+        }else if (salary <200000){
+            System.out.println("I am job holder");
+    }else if (salary <500000){
+            System.out.println("I am businessman");
+}else if (salary <1000000){
+            System.out.println("I am developer");
+}else{
+            System.out.println("I am something else");
+        }
+        sc.close();
+    }
+}
