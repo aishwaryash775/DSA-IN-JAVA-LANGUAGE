c6c098b8019054b2338600d5dd556592861aa47e
Aishwarya Shinde <Aishwaryash880@gmail.com>
2026-02-02 22:52:20 +0530

    functions

---
+++ function.java
@@ -0,0 +1,13 @@
+public class function {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+             sum();
+
+    }
+    public static void  sum(){
+        int a = 5;
+        int b = 10;
+        int result = a + b;
+        System.out.println("The sum is: " + result);
+    }
+}
