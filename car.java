675da10357be99d24cba13de148c98b65f18ca10
Aishwarya Shinde <Aishwaryash880@gmail.com>
2026-02-04 11:08:43 +0530

    oops

---
+++ car.java
@@ -0,0 +1,10 @@
+public class car {
+        int speed;
+        String color;
+
+        void drive(){
+            // driving logic here
+            System.out.println("The car is driving.");
+        }
+    }
+
---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        car myCar = new car();
+       myCar.speed = 100;
+       myCar.color = "Red";
+       
+        myCar.drive();
+    }
+}
