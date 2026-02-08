e40782666f931771afbc4a0c3aa860429638b1b3
AISHWARYA SANTOSH SHINDE <aishwaryash880@gmail.com>
2026-02-08 17:33:16 +0530

    Clean up Main.java by removing comments and adding newline
    Removed commented-out ArrayExample class and added newline at end of file.

--- Main.java
+++ Main.java
@@ -10,3 +10,24 @@ public class Main{
       input.close();
     } 
 }
+
+
+
+// public class ArrayExample {
+//     public static void main(String[] args) {
+//         // Declaration and initialization
+//         int[] numbers = new int[5];
+
+//         // Insertion
+//         numbers[0] = 10;
+//         numbers[1] = 20;
+//         numbers[2] = 30;
+//         numbers[3] = 40;
+//         numbers[4] = 50;
+
+//         // Traversal
+//         for (int i = 0; i < numbers.length; i++) {
+//             System.out.println("Element at index " + i + ": " + numbers[i]);
+//         }
+//     }
+// }
